6333ccf0562a268f1a4a1c52769b6b30057a1924
Igor Dmitriev <idmitrievnz@gmail.com>
2024-06-13 13:31:03 +1200

    Added card view for users

--- app/src/main/java/com/example/githubapicompose/MainActivity.kt
+++ app/src/main/java/com/example/githubapicompose/MainActivity.kt
@@ -1,16 +1,29 @@
 package com.example.githubapicompose
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Card
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import com.example.githubapicompose.ui.theme.GitHubApiComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,21 +32,62 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             GitHubApiComposeTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    MainScreen(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                HomeScreen()
             }
         }
     }
 }
 
 @Composable
-fun MainScreen(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
+fun HomeScreen() {
+    Users(userLogin = "User login")
+}
+
+@Composable
+fun UserCard(userLogin: String, modifier: Modifier = Modifier) {
+    val context = LocalContext.current
+    Card(
         modifier = modifier
+            .fillMaxWidth()
+            .height(150.dp)
+            .padding(5.dp)
+            .clickable {
+                Toast
+                    .makeText(context, userLogin, Toast.LENGTH_SHORT)
+                    .show()
+            }
     )
+    {
+        //card content bellow
+        Row {
+            Image(
+                painter = painterResource(id = R.drawable.ic_launcher_foreground),
+                contentDescription = "",
+                contentScale = ContentScale.Fit,
+                modifier = Modifier
+                    .fillMaxWidth(0.4f)
+                    .fillMaxHeight()
+                    .padding(5.dp)
+            )
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+            ) {
+                Text(
+                    modifier = Modifier
+                        .align(Alignment.Center),
+                    text = userLogin
+                )
+            }
+        }
+    }//card content ends
+}
+
+@Composable
+fun Users(userLogin: String) {
+    LazyColumn {
+        items(30) { index ->
+            UserCard("$userLogin $index")
+        }
+    }
 }
