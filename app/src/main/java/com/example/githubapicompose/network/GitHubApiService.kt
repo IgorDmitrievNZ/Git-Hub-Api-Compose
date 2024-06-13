c2064281b7a3a9b8caac2757d914bea6736f5518
Igor Dmitriev <idmitrievnz@gmail.com>
2024-06-13 18:01:43 +1200

    Added Api service file

---
+++ app/src/main/java/com/example/githubapicompose/network/GitHubApiService.kt
@@ -0,0 +1,35 @@
+package com.example.githubapicompose.network
+
+import com.example.githubapicompose.model.UserDTO
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType.Companion.toMediaType
+import retrofit2.Retrofit
+import retrofit2.http.GET
+
+private const val BASE_URL = "https://api.github.com/"
+
+/**
+ * Use the Retrofit builder to build a retrofit object using a kotlinx.serialization converter
+ */
+private val retrofit = Retrofit.Builder()
+    .addConverterFactory(Json.asConverterFactory("application/json".toMediaType()))
+    .baseUrl(BASE_URL)
+    .build()
+
+/**
+ * Retrofit service object for creating api calls
+ */
+interface UsersApiService {
+    @GET("users")
+    suspend fun getUsers(): List<UserDTO>
+}
+
+/**
+ * A public Api object that exposes the lazy-initialized Retrofit service
+ */
+object UsersApi {
+    val retrofitService: UsersApiService by lazy {
+        retrofit.create(UsersApiService::class.java)
+    }
+}
