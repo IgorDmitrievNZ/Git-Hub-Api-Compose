8a2f7032e49a768dd978012321ef8793ff570db1
Igor Dmitriev <idmitrievnz@gmail.com>
2024-06-13 18:03:46 +1200

    Added ui components to display users

--- app/src/main/java/com/example/githubapicompose/MainActivity.kt
+++ app/src/main/java/com/example/githubapicompose/MainActivity.kt
@@ -7,15 +7,20 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Card
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -24,6 +29,11 @@ import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import coil.compose.AsyncImage
+import com.example.githubapicompose.model.UserDTO
+import com.example.githubapicompose.ui.screens.HomeUiState
+import com.example.githubapicompose.ui.screens.HomeViewModel
 import com.example.githubapicompose.ui.theme.GitHubApiComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -32,19 +42,28 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             GitHubApiComposeTheme {
-                HomeScreen()
+                val homeViewModel: HomeViewModel = viewModel()
+                HomeScreen(
+                    homeUiState = homeViewModel.homeUiState,
+                    modifier = Modifier.fillMaxSize()
+                )
             }
         }
     }
 }
 
 @Composable
-fun HomeScreen() {
-    Users(userLogin = "User login")
+fun HomeScreen(homeUiState: HomeUiState, modifier: Modifier) {
+
+    when (homeUiState) {
+        is HomeUiState.Loading -> LoadingScreen()
+        is HomeUiState.Success -> Users(homeUiState.users)
+        is HomeUiState.Error -> ErrorScreen(modifier = modifier.fillMaxSize())
+    }
 }
 
 @Composable
-fun UserCard(userLogin: String, modifier: Modifier = Modifier) {
+fun UserCard(user: UserDTO, modifier: Modifier = Modifier) {
     val context = LocalContext.current
     Card(
         modifier = modifier
@@ -53,16 +72,17 @@ fun UserCard(userLogin: String, modifier: Modifier = Modifier) {
             .padding(5.dp)
             .clickable {
                 Toast
-                    .makeText(context, userLogin, Toast.LENGTH_SHORT)
+                    .makeText(context, user.login, Toast.LENGTH_SHORT)
                     .show()
             }
     )
     {
         //card content bellow
         Row {
-            Image(
-                painter = painterResource(id = R.drawable.ic_launcher_foreground),
-                contentDescription = "",
+            AsyncImage(
+                model = user.avatar_url,
+                placeholder = painterResource(R.drawable.ic_launcher_foreground),
+                contentDescription = null,
                 contentScale = ContentScale.Fit,
                 modifier = Modifier
                     .fillMaxWidth(0.4f)
@@ -76,7 +96,7 @@ fun UserCard(userLogin: String, modifier: Modifier = Modifier) {
                 Text(
                     modifier = Modifier
                         .align(Alignment.Center),
-                    text = userLogin
+                    text = user.login
                 )
             }
         }
@@ -84,10 +104,38 @@ fun UserCard(userLogin: String, modifier: Modifier = Modifier) {
 }
 
 @Composable
-fun Users(userLogin: String) {
+fun Users(users: List<UserDTO>) {
     LazyColumn {
-        items(30) { index ->
-            UserCard("$userLogin $index")
+        items(users) { item ->
+            UserCard(user = item, modifier = Modifier)
         }
+    }
+}
+
+@Composable
+fun ErrorScreen(modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier,
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.ic_launcher_foreground),
+            contentDescription = ""
+        )
+        Text(text = "loading failed", modifier = Modifier.padding(16.dp))
+    }
+}
+
+@Composable
+fun LoadingScreen() {
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = Modifier.fillMaxSize()
+    ) {
+        CircularProgressIndicator(
+            modifier = Modifier
+                .width(100.dp)
+        )
     }
 }
