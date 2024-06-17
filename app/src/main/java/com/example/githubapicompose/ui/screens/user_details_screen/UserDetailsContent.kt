63625e0ee834b155e8a5ef5f4e9762f54d8821a0
Igor Dmitriev <idmitrievnz@gmail.com>
2024-06-18 09:56:08 +1200

    +

--- app/src/main/java/com/example/githubapicompose/ui/screens/user_details_screen/UserDetailsContent.kt
+++ app/src/main/java/com/example/githubapicompose/ui/screens/user_details_screen/UserDetailsContent.kt
@@ -1,8 +1,22 @@
 package com.example.githubapicompose.ui.screens.user_details_screen
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import coil.compose.AsyncImage
 
 @Composable
 fun UserDetailsContent() {
-
+    Column {
+        Text(text = "Login")
+        Text(text = "Name")
+        Text(text = "Location")
+        AsyncImage(model = "", contentDescription = "")
+        LazyRow {
+            items(5) {
+                Text(text = "repos")
+            }
+        }
+    }
 }
--- app/src/main/java/com/example/githubapicompose/ui/screens/user_details_screen/UserDetailsScreen.kt
+++ app/src/main/java/com/example/githubapicompose/ui/screens/user_details_screen/UserDetailsScreen.kt
@@ -1,2 +1,8 @@
-package com.example.githubapicompose.ui.screens
+package com.example.githubapicompose.ui.screens.user_details_screen
 
+import androidx.compose.runtime.Composable
+
+@Composable
+fun UserDetailsScreen() {
+    UserDetailsContent()
+}
