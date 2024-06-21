63eb2b852fdd877f61f429bb8e16713fcf2c02af
Igor Dmitriev <idmitrievnz@gmail.com>
2024-06-21 12:34:10 +1200

    Fixed issue with no data

--- app/src/main/java/com/example/githubapicompose/model/search_dto/Item.kt
+++ app/src/main/java/com/example/githubapicompose/model/search_dto/Item.kt
@@ -20,6 +20,7 @@ data class Item(
     val score: Double?,
     val site_admin: Boolean?,
     val starred_url: String?,
+    val subscriptions_url: String?,
     val type: String?,
     val url: String?
 )
