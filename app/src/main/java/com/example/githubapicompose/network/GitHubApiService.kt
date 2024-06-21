8ba7fdb8d353694be20cc813b785a6ac029305bc
Igor Dmitriev <idmitrievnz@gmail.com>
2024-06-21 12:27:10 +1200

    Added get search function

--- app/src/main/java/com/example/githubapicompose/network/GitHubApiService.kt
+++ app/src/main/java/com/example/githubapicompose/network/GitHubApiService.kt
@@ -1,9 +1,11 @@
 package com.example.githubapicompose.network
 
+import com.example.githubapicompose.model.search_dto.SearchDTO
 import com.example.githubapicompose.model.user_details_dto.UserDetailsDTO
 import com.example.githubapicompose.model.users_dto.UserDTO
 import retrofit2.http.GET
 import retrofit2.http.Path
+import retrofit2.http.Query
 
 /**
  * Retrofit service object for creating api calls
@@ -13,5 +15,13 @@ interface GitHubApiService {
     suspend fun getUsers(): List<UserDTO>
 
     @GET("users/{userLogin}")
-    suspend fun getUserDetails(@Path("userLogin") userLogin: String): UserDetailsDTO
+    suspend fun getUserDetails(
+        @Path("userLogin") userLogin: String
+    ): UserDetailsDTO
+
+    @GET("search/users")
+    suspend fun getSearchResult(
+        @Query("q") q: String
+    ): SearchDTO
+
 }
