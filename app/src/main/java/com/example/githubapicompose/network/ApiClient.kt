d8e6bed389be8c8426e7a52a5bf67094144cec44
Igor Dmitriev <idmitrievnz@gmail.com>
2024-06-21 12:24:58 +1200

    Updated API client

--- app/src/main/java/com/example/githubapicompose/network/ApiClient.kt
+++ app/src/main/java/com/example/githubapicompose/network/ApiClient.kt
@@ -3,13 +3,7 @@ package com.example.githubapicompose.network
 /**
  * A public Api object that exposes the lazy-initialized Retrofit service
  */
-object DetailsApi {
-    val retrofitService: GitHubApiService by lazy {
-        NetworkDataSource.retrofit.create(GitHubApiService::class.java)
-    }
-}
-
-object UsersApi {
+object ApiClient {
     val retrofitService: GitHubApiService by lazy {
         NetworkDataSource.retrofit.create(GitHubApiService::class.java)
     }
