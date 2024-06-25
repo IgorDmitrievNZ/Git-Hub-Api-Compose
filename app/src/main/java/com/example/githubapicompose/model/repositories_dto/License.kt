3af7ec74786d0233fc47e672b85973b4a3e2f012
Igor Dmitriev <idmitrievnz@gmail.com>
2024-06-26 11:18:02 +1200

    Refactor RepoDTO

--- app/src/main/java/com/example/githubapicompose/model/repositories_dto/License.kt
+++ app/src/main/java/com/example/githubapicompose/model/repositories_dto/License.kt
@@ -1,12 +1,14 @@
 package com.example.githubapicompose.model.repositories_dto
 
+
+import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
 data class License(
     val key: String?,
     val name: String?,
-    val node_id: String?,
-    val spdx_id: String?,
+    @SerialName("node_id") val nodeId: String?,
+    @SerialName("spdx_id") val spdxId: String?,
     val url: String?
 )
